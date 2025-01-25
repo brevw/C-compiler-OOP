03d5a1b7e29bf2686fe7614ab259701e78914ac0
brevw <tlili.ahmed.mo@gmail.com>
2025-01-25 13:05:14 -0500

    C-files dump

---
+++ src/test/C-files/double_reference.c
@@ -0,0 +1,10 @@
+#include "minic-stdlib.h"
+
+int main(){
+    int a;
+    int* b;
+    int** c;
+    a = 10;
+    b = &a;
+    c = &b;
+}
---
+++ src/test/C-files/reserved_keyword_as_identifier.c
@@ -0,0 +1,11 @@
+#include "minic-stdlib.h"
+
+
+int main(){
+    int a;
+    int while;
+    char if;
+    a = 10;
+    while = 20;
+    return 0;
+}
