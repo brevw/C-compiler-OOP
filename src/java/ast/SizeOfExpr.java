529259b827284eb54ab750c1b343123f13ff3b59
brevw <tlili.ahmed.mo@gmail.com>
2025-02-10 22:46:09 -0500

    fix -> rename from type as it was shadowing super class variable

--- src/java/ast/SizeOfExpr.java
+++ src/java/ast/SizeOfExpr.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 public final class SizeOfExpr extends Expr{
 
-    public final Type type;
+    public final Type ofType;
 
-    public SizeOfExpr(Type type){
-        this.type = type;
+    public SizeOfExpr(Type ofType){
+        this.ofType = ofType;
     }
 
     public List<ASTNode> children(){
