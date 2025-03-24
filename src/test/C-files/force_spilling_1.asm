ca2750a2f7e617f9fe76208e275100bdfcc9146d
brevw <tlili.ahmed.mo@gmail.com>
2025-03-24 03:23:11 -0400

    dump files

---
+++ src/test/C-files/force_spilling_1.asm
@@ -0,0 +1,71 @@
+.data
+
+.text
+.globl main
+main:
+addi v0, $zero, 0
+addi v1, $zero, 0
+addi v2, $zero, 0
+addi v3, $zero, 0
+addi v4, $zero, 0
+addi v5, $zero, 0
+addi v6, $zero, 0
+addi v7, $zero, 0
+addi v8, $zero, 0
+addi v9, $zero, 0
+addi v10, $zero, 0
+addi v11, $zero, 0
+addi v12, $zero, 0
+addi v13, $zero, 0
+addi v14, $zero, 0
+addi v15, $zero, 0
+addi v16, $zero, 0
+addi v17, $zero, 0
+addi v18, $zero, 0
+addi v19, $zero, 0
+addi v20, $zero, 0
+addi v21, $zero, 0
+addi v22, $zero, 0
+addi v23, $zero, 0
+addi v24, $zero, 0
+addi v25, $zero, 0
+addi v26, $zero, 0
+addi v27, $zero, 0
+addi v28, $zero, 0
+addi v29, $zero, 0
+addi v30, $zero, 0
+
+addi v100, $zero, 2
+add v100, v100, v100
+
+addi v0, v0, 1
+addi v1, v1, 1
+addi v2, v2, 1
+addi v3, v3, 1
+addi v4, v4, 1
+addi v5, v5, 1
+addi v6, v6, 1
+addi v7, v7, 1
+addi v8, v8, 1
+addi v9, v9, 1
+addi v10, v10, 1
+addi v11, v11, 1
+addi v12, v12, 1
+addi v13, v13, 1
+addi v14, v14, 1
+addi v15, v15, 1
+addi v16, v16, 1
+addi v17, v17, 1
+addi v18, v18, 1
+addi v19, v19, 1
+addi v20, v20, 1
+addi v21, v21, 1
+addi v22, v22, 1
+addi v23, v23, 1
+addi v24, v24, 1
+addi v25, v25, 1
+addi v26, v26, 1
+addi v27, v27, 1
+addi v28, v28, 1
+addi v29, v29, 1
+addi v30, v30, 1
