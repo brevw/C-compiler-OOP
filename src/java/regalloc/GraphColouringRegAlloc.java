08fd2d27589ab91d260dd3ba65523cef432cda06
brevw <tlili.ahmed.mo@gmail.com>
2025-04-05 14:42:17 -0400

    adding more register
    where removed to force test spilling

--- src/java/regalloc/GraphColouringRegAlloc.java
+++ src/java/regalloc/GraphColouringRegAlloc.java
@@ -28,9 +28,9 @@ public class GraphColouringRegAlloc implements AssemblyPass {
         AssemblyProgram newProg = new CollapseLabels().visit(asmProgWithVirtualRegs); // fix edge case where multiple instructions point to the same instruction
         ArrayList<ArrayList<CFGraph.Node>> cfgs = new CFGraph(newProg).generateProgramCFGs();
         List<InterferenceGraph> iGraphs = InterferenceGraph.buildInterferenceGraphFromCFGs(cfgs);
-        //List<Register.Arch> availableColors = List.of(Register.Arch.t0, Register.Arch.t1, Register.Arch.t2, Register.Arch.t3, Register.Arch.t4, Register.Arch.t5, Register.Arch.t6, Register.Arch.t7, Register.Arch.t8, Register.Arch.t9
-        //                                            , Register.Arch.s0, Register.Arch.s1, Register.Arch.s2, Register.Arch.s3, Register.Arch.s4, Register.Arch.s5, Register.Arch.s6, Register.Arch.s7);
-        List<Register.Arch> availableColors = List.of(Register.Arch.t0, Register.Arch.t1, Register.Arch.t2, Register.Arch.t3);
+        List<Register.Arch> availableColors = List.of(Register.Arch.t0, Register.Arch.t1, Register.Arch.t2, Register.Arch.t3, Register.Arch.t4, Register.Arch.t5, Register.Arch.t6, Register.Arch.t7, Register.Arch.t8, Register.Arch.t9
+                                                    , Register.Arch.s0, Register.Arch.s1, Register.Arch.s2, Register.Arch.s3, Register.Arch.s4, Register.Arch.s5, Register.Arch.s6, Register.Arch.s7);
+        //List<Register.Arch> availableColors = List.of(Register.Arch.t0, Register.Arch.t1, Register.Arch.t2, Register.Arch.t3);
         GraphColor gc = new GraphColor(availableColors, newProg, iGraphs);
         gc.transformToPhysicalASMProgram();
         return newProg;
