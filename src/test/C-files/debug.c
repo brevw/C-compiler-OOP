45e1e5bee49080fbdf191b90c5bbe1497d210599
brevw <tlili.ahmed.mo@gmail.com>
2025-04-09 03:04:23 -0400

    test files dump

--- src/test/C-files/debug.c
+++ src/test/C-files/debug.c
@@ -1,42 +1,98 @@
 #include "minic-stdlib.h"
 
-class Animal {
-    int age;
-    void init(int a) {
-        age = a;
+int global_var;
+
+class Object {
+    int a;
+    void print() {
+        print_s((char*)"Instant Type Object: a -> ");
+        print_i(a);
+        print_s((char*)"\n");
     }
-    void speak() {
-        print_s((char*)"Animal sound\n");
+    void init1(int a_) {
+        a = a_;
     }
-    void printAge() {
-        print_s((char*)"Age: ");
-        print_i(age);
-        print_c('\n');
+    void doubleA() {
+         a = a * 2;
     }
 }
 
-class Dog extends Animal {
-    void speak() {
-        print_s((char*)"Bark\n");
+class Node extends Object {
+    int data;
+    char c;
+    void init2(int a_, int data_, char c_) {
+        a = a_;
+        data = data_;
+        c = c_;
     }
-}
-
-class Labrador extends Dog {
-    void speak() {
-        print_s((char*)"Labrador bark\n");
+    void print() {
+        print_s((char*)"Instant Type Node:");
+        print_s((char*)" a -> ");
+        print_i(a);
+        print_s((char*)", data -> ");
+        print_i(data);
+        print_s((char*)", c -> ");
+        print_c(c);
+        print_s((char*)"\n");
+    }
+    void troll() {
+        print_s((char*)"troll");
     }
 }
 
 void main() {
-    class Labrador lab;
-    class Animal a;
-    lab = new class Labrador();
+    class Object o_node;
+    class Object o_object;
+    class Node n_node;
+    o_node = (class Object) new class Node();
+    o_object = new class Object();
+    n_node = new class Node();
 
-    lab.init(5);
-    lab.printAge();  // From Animal
-    lab.speak();     // Should print "Labrador bark"
+    // funcall test
+    print_s((char*)"-- Setting a -> 1\n");
+    o_node.init1(1);
+    o_node.print();
+    print_c('\n');
 
-    // Test superclass casting
-    a = (class Animal)lab;
-    a.speak();       // Should still print "Labrador bark" (polymorphism)
+    print_s((char*)"-- Setting a -> 2\n");
+    o_object.init1(2);
+    o_object.print();
+    print_c('\n');
+
+    print_s((char*)"-- Setting a -> 3, data -> 4, c -> 'a'\n");
+    n_node.init2(3, 4, 'a');
+    n_node.print();
+    print_c('\n');
+
+    // funcall inheritance test
+    print_s((char*)"-- Double a: a -> 2\n");
+    o_node.doubleA();
+    o_node.print();
+    print_c('\n');
+
+    print_s((char*)"-- Double a: a -> 4\n");
+    o_object.doubleA();
+    o_object.print();
+    print_c('\n');
+
+    print_s((char*)"-- Double a: a -> 6\n");
+    n_node.doubleA();
+    n_node.print();
+    print_c('\n');
+
+    // field test
+    print_s((char*)"-- Setting a -> 10\n");
+    o_node.a = 10;
+    o_node.print();
+    print_c('\n');
+
+    print_s((char*)"-- Setting a -> 20\n");
+    o_object.a = 20;
+    o_object.print();
+    print_c('\n');
+
+    print_s((char*)"-- Setting a -> 30\n");
+    n_node.a = 30;
+    n_node.print();
+    print_c('\n');
 }
